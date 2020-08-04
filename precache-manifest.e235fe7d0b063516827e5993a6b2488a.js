e36a1688374093866f6317f2f9e87f4fd7fdb730
Ulf Wiren-Hallqvist <ulf.wirenhallqvist@usify.se>
2020-08-04 15:25:30 +0200

    Updates

---
+++ precache-manifest.e235fe7d0b063516827e5993a6b2488a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b76a7bc2c21785973e3c341b472dc26",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "05de7eca6ddfee98915a",
+    "url": "/monsters-rolodex/static/css/main.3526471f.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05de7eca6ddfee98915a",
+    "url": "/monsters-rolodex/static/js/main.d42f5fe3.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
